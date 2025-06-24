58905bddd0f384d82e140326efef086cca25f580
Beatriz-Cristina <151976407+Beatriz-Cristina@users.noreply.github.com>
2025-06-24 09:18:56 -0300

    Update Utils.cpp

--- Utils.cpp
+++ Utils.cpp
@@ -1,5 +1,6 @@
 #include "Utils.h"
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -23,6 +24,22 @@ int Utils::getIntInput(const string& prompt) {
             cin.clear();
             cin.ignore(256, '\n');
             printMessage("Attention, invalid input! Please enter a number.");
+        } else {
+            cin.ignore();
+            return value;
+        }
+    }
+}
+
+double Utils::getDoubleInput(const string& prompt) {
+    double value;
+    while (true) {
+        cout << prompt;
+        cin >> value;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            printMessage("Invalid input! Please enter a number.");
         } else {
             cin.ignore();
             return value;
